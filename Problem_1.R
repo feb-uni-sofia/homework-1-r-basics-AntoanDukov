40a8677128411f509480fda402c820fda35e5de0
AntoanDukov <37963017+AntoanDukov@users.noreply.github.com>
2018-04-02 08:20:47 +0000

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,26 @@
 
+#a) 
+x <- c(4,1,1,4)
+
+# b) 
+y <- c(1,4)
+
+# c) There are two vectors each of which has a different size.The recycled vector is the smaller one and it becomes c(1,4,1,4)
+x-y
+# d) 
+s <- c(x,y)
+# e)
+rep(s,10)
+srep <- rep(s,10)
+length(srep)
+# f)
+rep(s, each=3)
+# g)
+seq(7,21, by=1)
+# h)
+lseq <- seq(7,21, by=1)
+length(lseq)
+
+
+
+
